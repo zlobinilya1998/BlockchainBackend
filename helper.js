092730c719d6c96b27474b697e0ad69e1a098f3e
zlobin_ilya <qwertin1998@gmail.com>
2022-03-25 01:38:22 +0300

    --

--- helper.js
+++ helper.js
@@ -4,6 +4,7 @@ export const ErrorHandler = async (cb,res) => {
     try {
         const result = await cb();
         res.send(SuccessResult(result));
+        console.log(result)
     } catch (e) {
         res.status(400).send({message: 'Что-то пошло не так, попробуйте позже',e})
     }
