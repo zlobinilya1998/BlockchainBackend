18848c637a9b48ef0e645e0f57d7dfd085e6a271
zlobin_ilya <qwertin1998@gmail.com>
2022-03-25 00:18:15 +0300

    --

--- index.js
+++ index.js
@@ -27,18 +27,16 @@
 
 
 const app = require('express')();
-const { v4 } = require('uuid');
 
-app.get('/api', (req, res) => {
-    const path = `/api/item/${v4()}`;
-    res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
+const PORT = process.env.PORT || 5050;
 
-app.get('/api/item/:slug', (req, res) => {
-    const { slug } = req.params;
-    res.end(`Item: ${slug}`);
-});
+app.listen(PORT,() => {
+    console.log('server been started on port:',PORT)
+})
 
-module.exports = app;
+app.get('api/product',async (req,res) => {
+    res.send({
+        name: 'Boots',
+        price: 120
+    })
+})
